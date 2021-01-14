c9b3a400d89739c4fa6bf96a155be5b797ff625e
Austin Hernandez <ahernandez1170@gmail.com>
2021-01-14 00:42:12 +0000

    Automatically backed up by Learn

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -34,8 +34,7 @@ describe 'Deli Counter' do
 
     context "there are already people in line" do
       it "should add a person to the end of the line" do
-        # This weird looking %w syntax is just another way to instantiate an
-        # Array. %w(item1 item2) is equivalent to ["item1", "item2"]
+    
         expect($stdout).to receive(:puts).with("Welcome, Grace. You are number 4 in line.")
         take_a_number(other_deli, "Grace")
         expect(other_deli).to eq(%w(Logan Avi Spencer Grace))
